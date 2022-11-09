ed999dc0ffe57c8b8b918601886e44e0937a2da6
Wojtek-A <wojtek.a@hotmail.com>
2022-11-09 12:27:44 +0100

    Changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { gallery, images, lightbox } from './code-from-js-hw-07';
