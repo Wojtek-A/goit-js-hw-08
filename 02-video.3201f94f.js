571f23ee29a136f0e2eea3ec93aa91cdb3295efd
Wojtek-A <107140998+Wojtek-A@users.noreply.github.com>
2022-11-11 12:14:00 +0000

    Deploying to gh-pages from @ Wojtek-A/goit-js-hw-08@7856814352b1949d1e7a1adb069b1af8ae08471a 🚀

--- 02-video.3201f94f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",_.throttle((e=>{localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3));const n=parseInt(localStorage.getItem("videoplayer-current-time"));t.setCurrentTime(n).then((function(e){})).catch((function(e){e.name}));
-//# sourceMappingURL=02-video.3201f94f.js.map
