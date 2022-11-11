7856814352b1949d1e7a1adb069b1af8ae08471a
Wojtek-A <wojtek.a@hotmail.com>
2022-11-11 13:13:07 +0100

    Changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,8 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iFrame = document.querySelector('iframe');
 const player = new Vimeo.Player(iFrame);
-// const throttle = require('lodash.throttle');
 
 const savedTime = time => {
   localStorage.setItem('videoplayer-current-time', time.seconds);
@@ -10,8 +12,14 @@ player.on('timeupdate', _.throttle(savedTime, 1000));
 
 const timeStorage = parseInt(localStorage.getItem('videoplayer-current-time'));
 
+function nazwa(timeStorage) {
+  if (timeStorage < 571) {
+    return timeStorage;
+  } else return timeStorage === 0;
+}
+
 player
-  .setCurrentTime(timeStorage)
+  .setCurrentTime(nazwa(timeStorage))
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
