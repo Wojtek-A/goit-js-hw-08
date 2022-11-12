6ccaa325672fc2e593e7431d62ed81d11529c920
Wojtek-A <107140998+Wojtek-A@users.noreply.github.com>
2022-11-12 19:59:28 +0000

    Deploying to gh-pages from @ Wojtek-A/goit-js-hw-08@ae24b2dfd35d9d2150139d10bbaae08c1f798c3e 🚀

--- 03-feedback.bdffb890.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("input"),o=document.querySelector("textarea");console.log(e,o);
-//# sourceMappingURL=03-feedback.bdffb890.js.map
--- 03-feedback.c16746e4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("input"),o=document.querySelector("textarea");console.log(e,o)}();
-//# sourceMappingURL=03-feedback.c16746e4.js.map
