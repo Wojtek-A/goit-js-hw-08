ae24b2dfd35d9d2150139d10bbaae08c1f798c3e
Wojtek-A <wojtek.a@hotmail.com>
2022-11-12 20:58:52 +0100

    Task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,32 @@
+import throttle from 'lodash.throttle';
 
-const emailInput = document.querySelector("input")
-const message = document.querySelector("textarea")
-console.log(emailInput, message)
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+const dataHandler = () => {
+  const input = { mail: email.value, text: message.value };
+  const inputJSON = JSON.stringify(input);
+  localStorage.setItem('feedback-form-state', inputJSON);
+};
+
+[email, message].forEach(function (element) {
+  element.addEventListener('keyup', throttle(dataHandler, 500));
+});
+
+const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+const updateData = () => {
+  if (email.value === '' || message.value === '') {
+    email.value = data.mail;
+    message.value = data.text;
+  }
+};
+updateData();
+
+form.addEventListener('submit', saveMessage);
+function saveMessage(event) {
+  event.preventDefault();
+  form.reset();
+  localStorage.clear();
+}
